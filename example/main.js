dbb8ee99134d0a55f76edd7bb6481d3720a200eb
gdi2290 <github@gdi2290.com>
2013-09-22 15:50:01 -0700

    add /example code

---
+++ example/main.js
@@ -0,0 +1,101 @@
+// App Module
+angular.module('PubNubChat',[
+  'angular-pubnub',
+  'directives',
+  'controllers'
+]);
+
+// Directives Module
+angular.module('directives', [])
+  .directive('coolFade', function() {
+    return {
+      compile: function(elm) {
+        //console.log('compiling');
+        $(elm).css('opacity', 0);
+        return function(scope, elm, attrs) {
+         // console.log('animating');
+          $(elm).animate({ opacity : 1.0 }, 1000 );
+        };
+      }
+    };
+  });
+
+// Controllers Module
+angular.module('controllers', [])
+  .controller('MainCtrl', function($scope) {
+    $scope.messages = [];
+    $scope.realtimeStatus = "Connecting...";
+    $scope.channel = "pubnub_chat";
+    $scope.limit = 20;
+
+    //publish a chat message
+    $scope.publish = function(){
+
+        //toggle the progress bar
+        $('#progress_bar').slideToggle();
+
+         PUBNUB.publish({
+                channel : $scope.channel,
+                message : $scope.message
+            })
+
+        //reset the message text
+       $scope.message.text = "";
+    }
+
+    //gets the messages history
+    $scope.history = function(){
+        PUBNUB.history( {
+            channel : $scope.channel,
+            limit   : $scope.limit
+        }, function(messages) {
+            // Shows All Messages
+            $scope.$apply(function(){
+                $scope.messages = messages;
+
+            });
+        } );
+     }
+
+
+   //we'll leave these ones as is so that pubnub can
+   //automagically trigger the events
+   PUBNUB.subscribe({
+        channel    : $scope.channel,
+        restore    : false,
+
+        callback   : function(message) {
+
+            //toggle the progress_bar
+            $('#progress_bar').slideToggle();
+
+            $scope.$apply(function(){
+                $scope.messages.push(message);
+
+            });
+        },
+
+        disconnect : function() {
+            $scope.$apply(function(){
+                $scope.realtimeStatus = 'Disconnected';
+            });
+        },
+
+        reconnect  : function() {
+            $scope.$apply(function(){
+                $scope.realtimeStatus = 'Connected';
+            });
+        },
+
+        connect    : function() {
+            $scope.$apply(function(){
+                $scope.realtimeStatus = 'Connected';
+                //hide the progress bar
+                $('#progress_bar').slideToggle();
+                //load the message history from PubNub
+                $scope.history();
+            });
+
+        }
+    })
+  });
